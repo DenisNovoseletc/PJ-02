ed7035c1e1914dd4f1bd3a9950b5f93eee2aa81a
DenisNovoseletc <denicnovo@gmail.com>
2025-03-27 20:44:56 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,24 +1,52 @@
 const entities = [
-    {
-      img: 'img/image1.png'
-    },
-    {
+  {
+      text: {
+          text1: 'Rostov-on-Don<br>LCD admiral',
+          text2: '81 m2',
+          text3: '3.5 months',
+          text4: 'Upon request',
+      },
+      img: 'img/image1.png' 
+  },
+  {
+      text: {
+          text1: 'Sochi<br>Thieves',
+          text2: '105 m2',
+          text3: '4 months',
+          text4: 'Upon request',
+      },
       img: 'img/image2.png'
-    },
-    {
+  },
+  {
+      text: {
+          text1: 'Rostov-on-Don<br>Patriotic',
+          text2: '93 m2',
+          text3: '3 months',
+          text4: 'Upon request',
+      },
       img: 'img/image3.png'
-    }
-  ];
-  
+  }
+];
   const img = document.querySelector('.content');
   const dots = document.querySelectorAll('.dot'); 
   const links = document.querySelectorAll('.slider__link'); // Получаем все ссылки
   const prev = document.querySelector('.left-arrow');
   const next = document.querySelector('.right-arrow');
+  const textElements = [
+    document.querySelector('.sidebar__text-p1'),
+    document.querySelector('.sidebar__text-p2'),
+    document.querySelector('.sidebar__text-p3'),
+    document.querySelector('.sidebar__text-p4')
+];   
   let currentIndex = 0;
   
   
-  const setEntity = (index) => {
+  const setEntity = (index) => {   
+      const entity = entities[index]; // Получаем текущий объект из массива
+      // Обновляем тексты с помощью цикла
+      Object.values(entity.text).forEach((text, i) => {
+          textElements[i].innerHTML = text; // Устанавливаем текст для каждого элемента
+      });
     img.style.backgroundImage = `url(${entities[index].img})`;
     
     // Сбрасываем активные состояния для точек и ссылок
